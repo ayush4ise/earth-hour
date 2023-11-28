e6c9b6e7bfe4ba0d829a3118887830b2bfe4c73a
ayush4ise <pkumar46802@gmail.com>
2023-11-28 18:27:56 +0530

    robust method

--- utils.py
+++ utils.py
@@ -22,3 +22,35 @@ def earthhour(year):
     plt.xticks(rotation=45)
     plt.legend()
     plt.show()
+
+def robust_method(year):
+    """deseasonalizing all the saturdays of an year and then calculating the ratio"""
+    df = pd.read_excel(f'Yearly Energy Demand Data/System Demand (Actual)/{year}.xlsx', index_col=0)
+    df.columns = pd.to_datetime(df.columns, dayfirst=True)
+    earthhour = sum(df[str(year)+f'-{earthhours[year]} 00:00:00']['21:00':'21:30'])
+    saturdays = {}
+    for i in df.columns:
+        if i.weekday() == 5 and i.year == year:
+            saturdays[i] = sum(df[i]['21:00':'21:30'])
+    saturdays = pd.Series(saturdays)
+
+    i_sm = {1: 0.9579037796074662,
+        2: 0.9596186164365493,
+        3: 0.9898073559402792,
+        4: 1.0030397134620446,
+        5: 1.018392643199991,
+        6: 1.0261222064430728,
+        7: 1.02305328000198,
+        8: 1.017260959899729,
+        9: 1.020519376655325,
+        10: 1.011875522378184,
+        11: 0.9942104067376999,
+        12: 0.976059327973805}
+    
+    # deseasonalize the data
+    deseasonalized = saturdays.copy()
+    for i in deseasonalized.index:
+        deseasonalized[i] = deseasonalized[i] / i_sm[i.month]
+    deseasonalized_eh = earthhour / i_sm[3]
+
+    print('Ratio of Earth Hour '+str(year)+' to Average Saturday: ', deseasonalized_eh/deseasonalized.drop(str(year)+f'-{earthhours[year]} 00:00:00').mean())
